573b47c01af5dffc9039682bb09319034b239d55
Fokoda799 <fey2003@gmail.com>
2024-10-13 12:23:30 +0100

    Add BasicLiterals and HelloWorld classes

---
+++ BasicLiterals.java
@@ -0,0 +1,9 @@
+public class BasicLiterals {
+    public static void main(String[] args) {
+        int number = 100;
+        String greeting = "Hello";
+
+        System.out.println(number);
+        System.out.println(greeting);
+    }
+}
